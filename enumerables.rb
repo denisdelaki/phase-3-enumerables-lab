d0c116c6a6f79f4c6e95982ce4d43daf4bb7e288
Sieva-cmd <musyokasieva@gmail.com>
2022-08-18 12:48:34 +0300

    Done.

--- enumerables.rb
+++ enumerables.rb
@@ -13,12 +13,17 @@ end
 # with the names of each spicy food
 def get_names(spicy_foods)
   # your code here
+p spicy_foods.map{|food| food[:name]}
+  
+ 
 end
 
 # given an array of spicy foods, **return an array of hashes** 
 # where the heat level of the food is greater than 5
 def spiciest_foods(spicy_foods)
   # your code here
+  
+  p spicy_foods.filter{|level| level[:heat_level]>5}
 end
 
 # given an array of spicy foods, **output to the terminal**
@@ -28,18 +33,27 @@ end
 # "hello" * 3 == "hellohellohello"
 def print_spicy_foods(spicy_foods)
   # your code here
+
+    spicy_foods.map { |show| puts "#{show[:name]} (#{show[:cuisine]}) | Heat Level: #{"🌶" * show[:heat_level]}"}
+  
 end
 
 # given an array of spicy foods and a string representing a cuisine, **return a single hash**  
 # for the spicy food whose cuisine matches the cuisine being passed to the method
 def get_spicy_food_by_cuisine(spicy_foods, cuisine)
   # your code here
+  spicy_foods.find do |cuis|
+    cuis[:cuisine]==cuisine
+  end
 end
 
 # Given an array of spicy foods, **return an array of hashes** 
 # sorted by heat level from lowest to highest
 def sort_by_heat(spicy_foods)
   # your code here
+  spicy_foods.sort_by do |sorted_food|
+    sorted_food[:heat_level]
+  end
 end
 
 # given an array of spicy foods, output to the terminal ONLY 
@@ -47,11 +61,15 @@ end
 # Buffalo Wings (American) | Heat Level: 🌶🌶🌶
 # HINT: Try to use methods you've already written to solve this!
 def print_spiciest_foods(spicy_foods)
-  # your code here
+  spicy_foods.map do |show| if show[:heat_level] > 5 
+    puts "#{show[:name]} (#{show[:cuisine]}) | Heat Level: #{"🌶" * show[:heat_level]}"
+end
+end
 end
 
 # given an array of spicy foods, return an integer representing 
 # the average heat level of all the spicy foods in the array
 def average_heat_level(spicy_foods)
   # your code here
+  spicy_foods.sum {|food| food[:heat_level]/spicy_foods.size}
 end
